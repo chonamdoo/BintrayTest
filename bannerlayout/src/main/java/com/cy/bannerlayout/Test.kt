e51b83d7de861c5fc724d30235facf17fc4050d7
chonamdoo <psymarine@gmail.com>
2017-08-13 18:14:53 +0900

    테스트

---
+++ bannerlayout/src/main/java/com/cy/bannerlayout/Test.kt
@@ -0,0 +1,14 @@
+package com.cy.bannerlayout
+
+import android.content.Context
+import android.widget.Toast
+
+/**
+ * Created by chonamdoo on 2017. 8. 13..
+ */
+
+object Test{
+    fun test(context: Context){
+        Toast.makeText(context,"라이브러리 테스트",Toast.LENGTH_LONG).show()
+    }
+}
